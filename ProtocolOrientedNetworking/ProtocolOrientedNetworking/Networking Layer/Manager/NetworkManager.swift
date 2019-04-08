a5b889d6db057a68562d8b41a0ec7dc337e87364
Matthew Harrilal <matthewharrilal@gmail.com>
2019-04-08 12:30:22 -0700

    Populated the values of the network response enum

--- ProtocolOrientedNetworking/ProtocolOrientedNetworking/Networking Layer/Manager/NetworkManager.swift
+++ ProtocolOrientedNetworking/ProtocolOrientedNetworking/Networking Layer/Manager/NetworkManager.swift
@@ -16,10 +16,10 @@ public struct NetworkManager {
 
 public enum NetworkResponse: String {
     case success = "Network call was a success"
-    case authenticationError
-    case badRequest
-    case outdated
-    case failed
-    case noData
-    case unableToDecode
+    case authenticationError = "You need to be authenticated first"
+    case badRequest = "Bad request"
+    case outdated = "The url you requested is outdated"
+    case failed = "The network call resulted in a failure"
+    case noData = "The network call resulted in no data being sent back"
+    case unableToDecode = "Unable to decode the response"
 }
