9a97c8e7b32ed6f597826aa3350d658a9590ceb2
Matthew Harrilal <matthewharrilal@gmail.com>
2019-04-08 12:22:33 -0700

    Added initalizers for the decodable structs ... completing the models

--- ProtocolOrientedNetworking/ProtocolOrientedNetworking/Model/Pokemon.swift
+++ ProtocolOrientedNetworking/ProtocolOrientedNetworking/Model/Pokemon.swift
@@ -14,20 +14,72 @@ struct Pokemon {
     var baseExperience: Int
     var forms: [Form]
     var height: Int
+    
 }
 
 
-struct Abilities {
+struct Abilities: Decodable {
     var ability: Ability
     var isHidden: Bool
     var slot: Int
+    
+    private enum AbilitiesKeys: String, CodingKey {
+        case ability
+        case isHidden = "is_hidden"
+        case slot
+    }
 }
 
-struct Form {
+struct Form: Decodable {
     var form: [Ability]
+    
+    private enum FormKeys: String, CodingKey {
+        case form
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: FormKeys.self)
+        form = try container.decode([Ability].self, forKey: .form)
+    }
 }
 
-struct Ability {
+struct Ability: Decodable {
     var name: String
     var url: String
+    
+    private enum AbilityKeys: String, CodingKey {
+        case name
+        case url
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: AbilityKeys.self)
+        
+        name = try container.decode(String.self, forKey: .name)
+        url = try container.decode(String.self, forKey: .url)
+    }
+}
+
+
+extension Pokemon: Decodable {
+    private enum PokemonKeys: String, CodingKey {
+        case abilities
+        
+        case baseExperience = "base_experience"
+        
+        case forms
+        
+        case height
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: PokemonKeys.self)
+        let abilities = try container.decode([Abilities].self, forKey: .abilities)
+        let baseExperience = try container.decode(Int.self, forKey: .baseExperience)
+        let forms = try container.decode([Form].self, forKey: .forms)
+        let height = try container.decode(Int.self, forKey: .height)
+        
+        // Top level structure not being referenced therefore need to initialize
+        self.init(abilities: abilities, baseExperience: baseExperience, forms: forms, height: height)
+    }
 }
