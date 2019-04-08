bd0b2804746a95a9962cc06d5cba27542d983e9f
Matthew Harrilal <matthewharrilal@gmail.com>
2019-04-08 10:54:09 -0700

    Implemented the encode functionality for the url parameters and the body parameters that we coded earlier

--- ProtocolOrientedNetworking/ProtocolOrientedNetworking/Networking Layer/Service/Router.swift
+++ ProtocolOrientedNetworking/ProtocolOrientedNetworking/Networking Layer/Service/Router.swift
@@ -13,12 +13,55 @@ class Router<EndPoint: EndpointType>: NetworkRouter {
     private var task: URLSessionTask?  // We do not want other files accessing our task
     
     func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion) {
+        let session = URLSession.shared
+        
+        do {
+            let request = try self.buildRequest(from: route)
+        }
+            
+        catch {
+            
+        }
         
     }
     
     func cancel() {
-        <#code#>
+        
     }
     
+    
+    fileprivate class func buildRequest(from route: EndPoint) throws -> URLRequest {
+        var request = URLRequest(url: route.baseUrl.appendingPathComponent(route.path), cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10.0)
+        
+        request.httpMethod = route.httpMethod
+        
+        do {
+            switch (route.task) {  // Depending on the additional configurations being sent with the request
+            case .request:
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                
+            case .requestParameters(let bodyParameters, let urlParameters):
+                try self.configute
+                
+            case .requestParametersandHeaders(let bodyParameters, let urlParameters, let headers):
+                
+            }
+        }
+    }
+    
+    fileprivate func configureParameters(bodyParameters: Parameters?, urlParameters: Parameters?, request: inout URLRequest) throws {
+        do {
+            if let bodyParameters = bodyParameters {
+                try JSONParameterEncoder.encode(urlRequest: &request, with: bodyParameters)
+            }
+            
+            if let urlParameters = urlParameters {
+                try URLParameterEncoder.encode(urlRequest: &request, with: urlParameters)
+            }
+        }
+        catch {
+            throw error
+        }
+    }
     
 }
