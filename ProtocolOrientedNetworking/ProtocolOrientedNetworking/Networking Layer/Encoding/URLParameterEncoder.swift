85b4923affb16e5174ae0a7802e2a59d5a5b1196
Matthew Harrilal <matthewharrilal@gmail.com>
2019-04-08 10:22:32 -0700

    Most recent push of parameter sanitization and attachement

--- ProtocolOrientedNetworking/ProtocolOrientedNetworking/Networking Layer/Encoding/URLParameterEncoder.swift
+++ ProtocolOrientedNetworking/ProtocolOrientedNetworking/Networking Layer/Encoding/URLParameterEncoder.swift
@@ -9,11 +9,15 @@
 import Foundation
 
 public struct URLParameterEncoder: ParameterEncoder {
+    
+    // Accepting request as a reference therefore changes will be made to the objects original spot in memory
     public static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        
         guard let url = urlRequest.url,
             var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
             else {throw NetworkError.missingURL} // Throw error if url is missing
         
+        // Only if there are parameters to concatenate 
         if  !parameters.isEmpty {
             
             urlComponents.queryItems = [URLQueryItem]() // Queuries contained on the url
@@ -23,6 +27,7 @@ public struct URLParameterEncoder: ParameterEncoder {
                 urlComponents.queryItems?.append(queryItem)
             }
             
+            // Update the url with the query items attached to the URL
             urlRequest.url = urlComponents.url
             
             if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
